879109a821401bf49c79561f066ce7561a4958cb
Jenkins <jenkins@ip-172-31-39-51.us-east-2.compute.internal>
2017-07-12 19:15:30 +0000

    Update TestBankAccount.java

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,4 +1,4 @@
-package ca.uwo.csd.cs2212.USERNAME;
+package ca.uwo.csd.cs2212.tpnguyen88;
 
 public class BankAccount {
 
--- src/TestBankAccount.java
+++ src/TestBankAccount.java
@@ -9,7 +9,7 @@ public class TestBankAccount {
   @Test
   public void testDebitWithSufficientFunds() {
     BankAccount account = new BankAccount(10);
-    double account = account.debit(5);
+    double amount = account.debit(5);
     Assert.assertEquals(5.0, amount);
   {
 }

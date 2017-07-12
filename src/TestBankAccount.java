2abde936cde120b9015cc823e80205aca56cc435
Jenkins <jenkins@ip-172-31-39-51.us-east-2.compute.internal>
2017-07-12 19:02:07 +0000

    Adds Jacoco plugin and a test

--- src/TestBankAccount.java
+++ src/TestBankAccount.java
@@ -1,4 +1,4 @@
-package ca.uwo.csd.cs2212.USERNAME;
+package ca.uwo.csd.cs2212.tpnguyen88;
 
 import junit.framework.Assert;
 import org.junit.Before;
@@ -6,4 +6,10 @@ import org.junit.Test;
 
 public class TestBankAccount {
 
+  @Test
+  public void testDebitWithSufficientFunds() {
+    BankAccount account = new BankAccount(10);
+    double account = account.debit(5);
+    Assert.assertEquals(5.0, amount);
+  {
 }
